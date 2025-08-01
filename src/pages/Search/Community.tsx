16fc63435c2e9ee84769eea9a7588d34a767ccfb
JadDavidIsReal <david.jad@icloud.com>
2025-08-01 21:37:51 +0800

    uploaded

---
+++ src/pages/Search/Community.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Community: React.FC = () => {
+    return <div>Community Page</div>;
+};
+
+export default Community;
