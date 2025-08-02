7f1000e517b9456927bbdd26c4dd11ce151e4e9a
JadDavidIsReal <david.jad@icloud.com>
2025-08-02 22:15:20 +0800

    Updated mock data + installed node modules
    STABLE VERSION
    Unfinished

--- src/mock/users.ts
+++ src/mock/users.ts
@@ -5,18 +5,18 @@ export const USERS = [
         username: 'nurse',
         password: 'password',
         role: ROLES.NURSE,
-        name: 'Florence Nightingale',
+        name: 'Maam UIC Nurse',
     },
     {
         username: 'doctor',
         password: 'password',
         role: ROLES.DOCTOR,
-        name: 'Dr. John Watson',
+        name: 'Doctor Jad David M.D.',
     },
     {
         username: 'patient',
         password: 'password',
         role: ROLES.PATIENT,
-        name: 'John Smith',
+        name: 'Pneu Monia',
     },
 ];
